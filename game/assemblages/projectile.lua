97df19fa5b0c0cabeee25aa299a1b26aa290ef80
Bee Akin <you@example.com>
2025-06-09 14:00:01 -0700

    better default projectile speed

--- game/assemblages/projectile.lua
+++ game/assemblages/projectile.lua
@@ -2,5 +2,5 @@ return function(e, x, y, w, h, onFinished)
     e
         :give("position", x, y)
         :give("size", w, h)
-        :give("projectile", nil, onFinished)
+        :give("projectile", 700, onFinished)
 end
